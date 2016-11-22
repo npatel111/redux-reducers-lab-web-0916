d7939274ea273526de0d2444b892c697aa03d4a5
Niti Nrw <niti.patel@flatironschool.com>
2016-11-22 11:23:59 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,26 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+  let newArray = state.slice()
 
-}
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [
+          ...state, action.payload]
+        // default: return state
+
+    case 'REMOVE_INGREDIENT':
+      // let new = [...state]
+      state.splice(action.payload, 1)
+       return state      // return newArray
+      // Object.assign({}, state, {
+      //    state.map((ingredient, index) =>{
+      //           if(state.index === action.index) {
+                    // state.slice(index, 1)
+
+                // }})
+                // return ingredients
+
+        // })
+        default:
+          return state
+        }
+      }
